66e658b5f4495d58f6049262ca099710d4d24277
Calvin Hsia <calvinh@microsoft.com>
2020-06-19 13:42:33 -0700

    wip

--- CreateDump/MainWindow.xaml.cs
+++ CreateDump/MainWindow.xaml.cs
@@ -31,30 +31,47 @@ namespace CreateDump
             await Task.Yield();
             this.Top = 0;
             this.Left = 0;
-            this.Width = 600;
+            this.Width = 800;
             this.Height = 500;
             try
             {
                 // use 32 bit task manager to dump 64 bit process. Result is quick exit with file length ==0
                 Debug.Assert(IntPtr.Size == 4, "in 32 bit proc");
                 var procToDump = "Microsoft.ServiceHub.Controller";
-                //                procToDump = "perfwatson2";
+//                procToDump = "perfwatson2";
                 var procs = Process.GetProcessesByName(procToDump);
                 if (procs.Length > 0)
                 {
                     var proc = procs[0];
                     var dumpFilename = Path.ChangeExtension(Path.GetTempFileName(), "dmp");
+                    var sw = Stopwatch.StartNew();
+                    Exception exception = null;
+                    var bitness = "32";
                     try
                     {
                         MemoryDumpHelper.CollectDump(proc, dumpFilename, fIncludeFullHeap: true);
                     }
-                    catch (Exception ex)
+                    catch (Exception ex) 
                     {
                         // PerfWatson is a 32 bit process, which can't get a dump of a 64 bit process
                         // we don't know for sure that a particular process is 64 bit (on 32 bit Windows, it may run in 32 bit mode)
                         // Capture a 64 bit dump on 32 bit windows
+                        // some exceptions are: {"Only part of a ReadProcessMemory or WriteProcessMemory request was completed. (Exception from HRESULT: 0x8007012B)"}
+                        exception = ex;
+                        bitness = "64";
                         Get64BitDump(proc, dumpFilename, fIncludeFullHeap: true);
                     }
+                    var exTxt = string.Empty;
+                    if (exception != null)
+                    {
+                        exTxt = $"Got exception when running 32 bit dump: {exception.GetType().Name} '{exception.Message}'";
+                    }
+                    this.Content = $@"
+                            Dumping Pid={proc.Id} {proc}
+                            {exTxt}
+                            {bitness} bit dump Size={new FileInfo(dumpFilename).Length:n0}  
+                            Secs= {sw.Elapsed.TotalSeconds:n1}
+                            DumpFile={dumpFilename}  ";
                 }
             }
             catch (Exception ex)
@@ -79,12 +96,12 @@ namespace CreateDump
                 {
                     Thread.Sleep(TimeSpan.FromSeconds(1));
                 }
-                this.Content = "procexited";
-
+                File.Delete(tempExeFileName);
             }
             catch (Exception)
             {
             }
+
         }
     }
 
---
+++ CreateDump64/MainProgram64.cs
@@ -0,0 +1,37 @@
+﻿using CreateDump;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CreateDump64
+{
+    public class MainProgram64
+    {
+        [STAThread]
+        public static void Main(string[] args) // [0] is 1st arg. array with 0 elems if no args
+        {
+            try
+            {
+                var arg2 = Environment.CommandLine; // entire cmd line as string
+                var arg3 = Environment.GetCommandLineArgs(); // [0] = fullpath to cur exe
+                var procToDump = Process.GetProcessById(int.Parse(args[0]));
+                MemoryDumpHelper.CollectDump(procToDump, args[1], fIncludeFullHeap: true);
+                //var x = new Window();
+                //var sb = new StringBuilder();
+                //sb.AppendLine($"in 64 bit process  IntPtr.Size = {IntPtr.Size} {Process.GetCurrentProcess().ProcessName}");
+                //sb.AppendLine(args.Length == 0 ? "No args" : string.Join(" ", args));
+                //x.Content = new TextBlock() { Text = sb.ToString() };
+                //x.ShowDialog();
+
+            }
+            catch (Exception)
+            {
+            }
+        }
+    }
+}
