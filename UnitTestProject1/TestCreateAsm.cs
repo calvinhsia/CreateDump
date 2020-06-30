97696bc0bb1a888c4eee731fde2a321e40d1aa96
Calvin Hsia <calvinh@microsoft.com>
2020-06-30 11:49:31 -0700

    createasm

---
+++ UnitTestProject1/TestCreateAsm.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class CreateAsmTests
+    {
+        public TestContext TestContext { get; set; }
+        [TestMethod]
+        public void TestCreateAsm()
+        {
+            // Create
+
+        }
+
+        public static void MyStaticMethod(string[] args)
+        {
+
+        }
+    }
+}
